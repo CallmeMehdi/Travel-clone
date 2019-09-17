65737c53e89b9d441768ea398baf367948b7b1ce
CallmeMehdi <47258917+CallmeMehdi@users.noreply.github.com>
2019-09-17 13:16:15 +0200

    Update script.js

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,28 +1,22 @@
 var elements = document.querySelectorAll(".fade-anim");
 var scrollElements = document.querySelectorAll(".fade-anim-scroll");
 
-console.log($(".ready").offset(),$(window).scrollTop())
 function fading(){
     for(i=0;i<elements.length;i++){
-        console.log($(window).scrollTop(),$(elements[i]).offset())
         if ($(window).scrollTop()>= elements[i].scrollTop){
-            console.log("here")
             $(elements[i]).fadeIn(1000);
         }
     }
 }
 
  $(document).ready(fading());
-//  console.log(scrollElements);
 
 setTimeout(function(){
     $(window).scroll(function(){
         let scroll = $(window).scrollTop()+ $(window).height();
         $(".fade-anim-scroll").each(function(){
-            console.log($(this).offset().top)
 
             if(scroll>=$(this).offset().top +$(this).outerHeight()){
-                console.log($(this))
                 $(this).animate({'opacity':'1'},1000);
 
             }
